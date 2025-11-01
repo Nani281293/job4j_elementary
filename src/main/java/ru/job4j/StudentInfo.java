7330073080ccd1ccf95cf2739a60a23730976e69
Иван <nani93@yandex.ru>
2025-11-01 20:17:15 +0300

    Фиксация изменений на github [#505270]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main (String[] args) {
         System.out.println("Ivan Brychin");
-        System.out.println("28.12.1993");
+        System.out.println("28.11.1993");
     }
 }
