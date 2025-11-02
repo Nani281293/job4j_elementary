c602651c56edce1a86dcb21cab9e6ba62c988371
Иван <nani93@yandex.ru>
2025-11-02 19:37:08 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279 #554164]

--- src/main/java/ru/job4j/calculator/TemperatureFit.java
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -1,13 +1,16 @@
 package ru.job4j.calculator;
 
 public class TemperatureFit {
-    public static double idealTemperatureForFruits (int temperature) {
+
+    public static double idealTemperatureForFruits(int temperature) {
         return temperature - 2.0;
     }
-    public static double idealTemperatureForMeat (int temperature) {
+
+    public static double idealTemperatureForMeat(int temperature) {
         return temperature - 5.0;
     }
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         int temperature = 10;
         double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
         double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
