6244a19a0cf53f7315a102d869bedae8ac57a3c0
Иван <nani93@yandex.ru>
2025-11-02 20:02:49 +0300

    Тестирование Класса Сложения Векторов [#505262 #554168]

--- src/test/java/ru/job4j/condition/VectorTest.java
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -9,7 +9,7 @@ class VectorTest {
     void whenVectors12And34ThenResult46() {
         int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
         String expected = "(4, 6)";
-        String output = Vector.addVectors(x1, y1, x2, y2);
+        String output = Vector.addVector(x1, y1, x2, y2);
         assertThat(output).isEqualTo(expected);
     }
 
@@ -17,7 +17,7 @@ class VectorTest {
     void whenVectorsMinus12And34ThenResult22() {
         int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
         String expected = "(2, 2)";
-        String output = Vector.addVectors(x1, y1, x2, y2);
+        String output = Vector.addVector(x1, y1, x2, y2);
         assertThat(output).isEqualTo(expected);
     }
 
@@ -25,7 +25,7 @@ class VectorTest {
     void whenVectorsMinus12AndMinus34ThenResult22() {
         int x1 = -1, y1 = -2, x2 = -3, y2 = -4;
         String expected = "(-4, -6)";
-        String output = Vector.addVectors(x1, y1, x2, y2);
+        String output = Vector.addVector(x1, y1, x2, y2);
         assertThat(output).isEqualTo(expected);
     }
 }
