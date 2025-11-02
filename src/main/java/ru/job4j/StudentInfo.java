fe775b8a5b2a010ad2fda61c07488caab22663cc
Иван <nani93@yandex.ru>
2025-11-02 19:51:20 +0300

    Разбор первой программы по "взрослому" [#505151 #554143]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -4,6 +4,6 @@ public class StudentInfo {
 
     public static void main(String[] args) {
         System.out.println("Ivan Brychin");
-        System.out.println("28.11.1993");
+        System.out.println("28.12.1993");
     }
 }
