988e88e614068cf46fe264804647b3b09eba93f3
Иван <nani93@yandex.ru>
2025-11-09 15:12:49 +0300

    AlgoArray - reformat code

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -3,17 +3,17 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-        int temp0 = array[0];
+        int temp = array[0];
         array[0] = array[3];
-        array[3] = temp0;
+        array[3] = temp;
 
-        int temp1 = array[1];
+        temp = array[1];
         array[1] = array[2];
-        array[2] = temp1;
+        array[2] = temp;
 
-        int temp3 = array[4];
+        temp = array[4];
         array[4] = array[3];
-        array[3] = temp3;
+        array[3] = temp;
 
         for (int element : array) {
             System.out.println(element);
