744821e97f90e09f97141d4e99737f98e3b252d8
Иван <nani93@yandex.ru>
2025-11-10 17:52:56 +0300

    Слово начинается с ... [#505233 #554563]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,18 +3,17 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
-        boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
             if (prefix[i] != word[i]) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 
     public static void main(String[] args) {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] prefix = {'H', 'e'};
+        char[] prefix = {'H', 'r', 'l'};
         boolean result = startsWith(word, prefix);
         System.out.println(result);
     }
