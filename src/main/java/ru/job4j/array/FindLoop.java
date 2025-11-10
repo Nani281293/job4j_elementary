e6a9dd127cf9951724c8dad9e5559d0e26b4928f
Иван <nani93@yandex.ru>
2025-11-10 18:37:28 +0300

    Поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,11 +1,23 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
+    public static int indexInRage(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static int index0f(int[] data, int element) {
         int result = -1;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == element) {
-                result = i;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == element) {
+                result = index;
                 break;
             }
         }
@@ -13,11 +25,18 @@ public class FindLoop {
     }
 
     public static void main(String[] args) {
-        int[] data = {0, 2, 1, 4};
-        int element = 4;
-        int index = index0f(data, element);
+        int[] data = {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int element = 8;
+        int start = 2;
+        int finish = 5;
+        int index = indexInRage(data, element, start, finish);
         System.out.println("Индекс элемента = " + index);
-}
+
+        data = new int[]{0, 2, 1, 4};
+        element = 4;
+        index = index0f(data, element);
+        System.out.println("Индекс элемента = " + index);
+    }
 }
 
 
